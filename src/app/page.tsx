197779a660e4c0e0634b0102d73e4bb63cca4f73
jeffjuann <jeffjuann@gmail.com>
2024-01-13 02:28:27 +0700

    dev: design page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-import { CopyButton } from '@/components/copy-button'
 import ResultContainer from '@/components/result-container'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import Image from 'next/image'
+import { randomUUID } from 'crypto'
 
 export default function Home()
 {
@@ -9,9 +8,12 @@ export default function Home()
     <main className="w-full h-full grid place-items-center p-12">
       <Card>
         <CardHeader>
-          <CardTitle>Page Unavailable</CardTitle>
-          <CardDescription>Sorry, this website is currently under development</CardDescription>
+          <CardTitle>UUID-GEN</CardTitle>
+          <CardDescription>Welcome to UUID-GEN. Your Ultimate Generator for UUID</CardDescription>
         </CardHeader>
+        <CardContent>
+          <ResultContainer value={randomUUID()}/>
+        </CardContent>
       </Card>
     </main>
   )
