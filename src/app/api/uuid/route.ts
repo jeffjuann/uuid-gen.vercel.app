c6125713d11ed381cfefa89c107f8794d786742d
jeffjuann <jeffjuann@gmail.com>
2024-01-17 02:15:36 +0700

    refactor(api): refactor response structure

--- src/app/api/uuid/route.ts
+++ src/app/api/uuid/route.ts
@@ -4,7 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function GET(req: NextRequest)
 {
-  const count: number = parseInt(req.nextUrl.searchParams.get('count') || '1');
-  const uuid: string | string[] = count === 1 ? randomUUID() : Array.from({ length: count }, () => randomUUID());
-  return NextResponse.json(uuid);
+  const count: number = parseInt(req.nextUrl.searchParams.get('count') ?? '');
+  if(req.nextUrl.searchParams.get('count') !== null)
+  {
+    const uuid: string[] = Array.from({ length: count }, () => randomUUID());
+    return NextResponse.json(uuid);
+  }
+  else
+  {
+    const uuid: string = randomUUID();
+    return NextResponse.json(uuid);
+  }
 }
